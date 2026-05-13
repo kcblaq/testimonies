bde62655aec0051b42d106e41bde30c8bd9b2070
Kelechi Ugwu <kcblack22@gmail.com>
2026-05-13 15:36:46 +0100

    Added yaml file for render deployment

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "generated/prisma/client";
+import bcrypt from 'bcrypt';
+import { PrismaService } from "src/prisma/prisma.service";
+
+
+async function createAdminUser() {
+    const prisma = new PrismaService();
+
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash('Pass@123', salt);
+
+        const adminUser = await prisma.admin.upsert({
+            where: {
+                email: 'kcblack22@gmail.com',
+            },
+            update: {
+                password: hashedPassword,
+            },
+            create: {
+                email: 'kcblack22@gmail.com',
+                password: hashedPassword,
+                name: 'Admin',
+            },
+        });
+        console.log(adminUser);
+    } catch (error) {
+        console.log(error);
+    }
+}
