9be6387aa671d05d6a16c7bb9839b1eb0040f5ab
Kelechi Ugwu <kcblack22@gmail.com>
2026-05-19 12:42:32 +0100

    Testing glob removal to see if it solves the issue with deployment

--- prisma/src/generated/prisma/models.ts
+++ prisma/src/generated/prisma/models.ts
@@ -8,7 +8,7 @@
  *
  * 🟢 You can import this file directly.
  */
-export type * from './models/Admin.js'
-export type * from './models/Testimony.js'
-export type * from './models/Category.js'
-export type * from './commonInputTypes.js'
+export type * from './models/Admin'
+export type * from './models/Testimony'
+export type * from './models/Category'
+export type * from './commonInputTypes'
