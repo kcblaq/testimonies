8076fc22fc09b3c4dfbd7c677f27c19f90fd681b
Kelechi Ugwu <kcblack22@gmail.com>
2026-05-20 15:17:14 +0100

    Corrected the seed file with the right import to prisma service

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 
 import bcrypt from 'bcrypt';
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../src/prisma/prisma.service";
 
 
 async function createAdminUser() {
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Welcome to NEST';
+    return 'Hello World! Welcome to FFI Testimonies API';
   }
 }
